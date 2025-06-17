22aa22c1ff3264bd22329049bfa549f30c9b5c08
Eduardo Ureta <eduardo.uretaca@gmail.com>
2025-06-17 19:41:04 -0400

    Primer commit para GH actions

---
+++ tests/app.test.js
@@ -0,0 +1,12 @@
+import { app } from "../src/app";
+import request from "supertest";
+
+describe('GET /api/inmuebles', () => {
+    it('debería retornar una lista de inmuebles', async () => {
+        const res = await request(app).get('/api/inmuebles');
+        expect(res.statusCode).toBe(200);
+        expect(res.body.length).toBeGreaterThan(0);
+        expect(res.body[0]).toHaveProperty('direccion');
+        expect(res.body[0]).toHaveProperty('precio');
+    });
+});
